4cd6194486d8bb9ab71ff36bbd6868ae5fb924a7
Koko <78096367+UselessNode@users.noreply.github.com>
2023-02-03 10:17:33 +0700

    Инициализация классов для хранения данных из базы данных

---
+++ Model/Forwarder.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Praxix.Model
+{
+    internal class Forwarder
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Supplier SupplyCompanyId { get; set; }
+    }
+}
---
+++ Model/Invoice.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Praxix.Model
+{
+    internal class Invoice
+    {
+        public int Id { get; set; }
+        public DateTime DepartureDate { get; set; }
+        public DateTime ArrivalDate { get; set; }
+
+        public Supplier SuplierId { get; set; }
+        public Product ProductId { get; set; }
+
+        public double Volume { get; set; }
+        public double Price { get; set; }
+    }
+}
